8f70ee04ee699d771bbab79000d3529fbc7e2a40
amindridi1 <125443150+amindridi1@users.noreply.github.com>
2024-10-30 21:41:57 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ def index():
 def handle_message(msg):
     send(msg, broadcast=True)  # Broadcast to all clients
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=8000)
